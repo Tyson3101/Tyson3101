76e4504b0a226979cce66657e51cdd2e3c62ee1e
Tyson3101 <67045138+Tyson3101@users.noreply.github.com>
2022-10-16 21:43:06 +1100

    Updates

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,40 @@
 <script lang="ts">
+  import { fly, fade } from "svelte/transition";
+  import TypescriptIcon from "./assets/icons/tools/Typescript.svelte";
+  import JavascriptIcon from "./assets/icons/tools/Javascript.svelte";
+  import HTMLIcon from "./assets/icons/tools/HTML.svelte";
+  import CSSIcon from "./assets/icons/tools/CSS.svelte";
+  import NodeJsIcon from "./assets/icons/tools/NodeJs.svelte";
+  import Navbar from "./lib/Navbar.svelte";
   console.log("Tyson3101");
 </script>
 
-<h1>Tyson3101</h1>
+<main>
+  <Navbar />
+  <section class="index">
+    <p transition:fade={{ duration: 1000, delay: 100 }} class="info">
+      Hi, I am Tyson. I live in Melbourne Australia!
+    </p>
+    <div>
+      <h4 transition:fly={{ x: -1000, duration: 2000 }}>Latest Video</h4>
+      <iframe
+        transition:fly={{ x: 2000, duration: 2000, delay: 300 }}
+        width="560"
+        height="315"
+        src="https://www.youtube.com/embed/SEwyQpfAZ54"
+        title="YouTube video player"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowfullscreen
+      />
+    </div>
+    <div>
+      <h5>Tools I am familer with:</h5>
+      <TypescriptIcon />
+      <JavascriptIcon />
+      <HTMLIcon />
+      <CSSIcon />
+      <NodeJsIcon />
+    </div>
+  </section>
+</main>
---
+++ src/lib/Navbar.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  import Icon from "svelte-icons-pack/Icon.svelte";
+  import AiFillYoutube from "svelte-icons-pack/ai/AiFillYoutube";
+  import AiFillGithub from "svelte-icons-pack/ai/AiFillGithub";
+  console.log("Tyson3101");
+</script>
+
+<header>
+  <h1>Tyson3101</h1>
+  <div>
+    <a
+      target="_blank"
+      href="https://www.youtube.com/channel/UCDhbvtFFtYvv9kH-kcdVsWg"
+    >
+      <Icon src={AiFillYoutube} color={"red"} /></a
+    >
+    {">"}
+    <a target="_blank" href="https://github.com/Tyson3101">
+      <Icon src={AiFillGithub} color={"white"} /></a
+    >
+  </div>
+</header>
