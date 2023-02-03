941a3f9266d5a611199dc3ce3450ca038661a297
Tyson3101 <67045138+Tyson3101@users.noreply.github.com>
2023-02-04 00:31:34 +1100

    Update latest video

--- src/App.svelte
+++ src/App.svelte
@@ -24,7 +24,7 @@
       <iframe
         width="560"
         height="315"
-        src="https://www.youtube.com/embed/SEwyQpfAZ54"
+        src="https://www.youtube.com/embed/JCFk_F1M1-A"
         title="YouTube video player"
         frameborder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
